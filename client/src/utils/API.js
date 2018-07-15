96af9867435804cb7840017575b61c50e65c373d
Alexander <agonz519@gmail.com>
2018-07-15 14:18:43 -0400

    Authentication in progress

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,1 +1,11 @@
-// space for eventual util functions
+import axios from "axios";
+
+export default {
+  // Saves a user to database
+  saveUser: (userData) => {
+    return axios.post("/api/user", userData);
+  },
+  loginUser: (userData) => {
+    return axios.post("/api/user/login", userData);
+  }
+};
--- server.js
+++ server.js
@@ -2,20 +2,46 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const express = require("express");
 const routes = require("./routes");
+const session = require("express-session");
+const passport = require("./passport");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// middleware
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+
 
 // Serve up static assets (heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+// middleware
+app.use(bodyParser.urlencoded({ useNewUrlParser: true, extended: true }));
+app.use(bodyParser.json());
+
 // Add routes, both API & view
 app.use(routes);
+
+// Sessions
+app.use(
+  session({
+    secret: "code-dictator",
+
+    resave: false, // required
+
+    saveUninitialized: false // required
+  })
+);
+
+app.use( (req, res, next) => {
+  console.log('req.session', req.session);
+  return next();
+});
+
+// Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/tablear";
 
