5fa4010162aa6662990ae14ff52a5a5e8c858f5c
John yeager <johnyeager@Johns-MBP.attlocal.net>
2018-07-21 12:29:30 -0400

    worked on navbar routes

--- client/src/pages/Intro/Intro.js
+++ client/src/pages/Intro/Intro.js
@@ -13,13 +13,13 @@ const Intro = () => (
       <h4 id="two">EFFECTIVE TASK MANAGEMENT</h4>
       <h2 id="three">IN AUGMENTED REALITY</h2>
       <br/>
-      <button type="button" to={"/login/"} className="introButton btn btn-secondary btn-lg">
-        <Link id="enter" to={"/login/"}>
+      <button type="button" to={"/login"} className="introButton btn btn-secondary btn-lg">
+        <Link id="enter" to={"/login"}>
           ENTER
         </Link>
       </button>
-      <button type="button" to={"/signUp/"} className="introButton btn btn-secondary btn-lg">
-        <Link id="enter" to={"/signUp/"}>
+      <button type="button" to={"/signUp"} className="introButton btn btn-secondary btn-lg">
+        <Link id="enter" to={"/signUp"}>
           SIGN UP
         </Link>
       </button>
