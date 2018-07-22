d5bc905a2c5dff73bd28072bbf3d3dc970dd9f2a
Geoff-Goodwin-Dev <ggoodwin.dev@gmail.com>
2018-07-22 13:26:11 -0400

    patches camera origin

--- client/src/components/WebCam/WebCam.js
+++ client/src/components/WebCam/WebCam.js
@@ -1,20 +1,12 @@
 import React, {Component} from "react";
 
 class WebCam extends Component {
-  state = {
-    videoSrc: null
-  };
-
   componentDidMount() {
-    // navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
-    // if (navigator.getUserMedia) {
-    //   navigator.getUserMedia({video: true}, this.handleVideo, this.videoError);
-    // }
     const video = document.querySelector('video');
     navigator.mediaDevices.getUserMedia = navigator.mediaDevices.getUserMedia || navigator.mediaDevices.webkitGetUserMedia || navigator.mediaDevices.mozGetUserMedia || navigator.mediaDevices.msGetUserMedia || navigator.mediaDevices.oGetUserMedia;
     if (navigator.mediaDevices.getUserMedia) {
       navigator.mediaDevices.getUserMedia({
-        video: true,
+        video: { facingMode: 'environment' },
         audio: false
       })
       .then((stream) => {
@@ -25,31 +17,6 @@ class WebCam extends Component {
       });
     }
   };
-
-  //
-  //
-  //
-  // handleVideo = (stream) => {
-  //   // Update the state, triggering the component to re-render with the correct stream
-  //   this.setState({videoSrc: window.URL.createObjectURL(stream)});
-  //   console.log('video feed initiated');
-  //   const video = document.querySelector('video');
-  //   const promise = video.play();
-  //   // promise won’t be defined in browsers that don't support promisified play()
-  //   if (promise === undefined) {
-  //     console.log('Promisified video play() not supported');
-  //   } else {
-  //     promise.then(() => {
-  //       console.log('Video playback successfully initiated, returning a promise');
-  //     }).catch((error) => {
-  //       console.log('Error initiating video playback: ', error);
-  //     });
-  //   }
-  // };
-  //
-  // videoError = () => {
-  //   console.log('video error encountered');
-  // };
 
   render() {
     return (
