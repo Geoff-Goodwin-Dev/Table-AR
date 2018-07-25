4dcb424f09d938c9997acd772dccb1664e8aa86c
Geoff-Goodwin-Dev <ggoodwin.dev@gmail.com>
2018-07-25 19:42:07 -0400

    refactors signup element and addcube

--- client/src/components/AddBlock/AddBlock.js
+++ client/src/components/AddBlock/AddBlock.js
@@ -1,63 +1,63 @@
 import React from "react";
 import {Entity} from 'aframe-react';
 
-const AddBlock = props => (
+const AddBlock = () => (
+  <Entity
+    id='addBlock'
+    className="clickable"
+    geometry={{
+      primitive: 'box',
+      depth: 0.25,
+      height: 0.5,
+      width: 0.5
+    }}
+    position="-2 2.75 0"
+    material={{
+      color: 'green',
+      opacity: 0.3,
+      side: 'double'
+    }}
+    shadow='receive: true;'
+  >
     <Entity
-      id='addBlock'
-      className="clickable"
-      geometry={{
-        primitive: 'box',
-        depth: 0.25,
-        height: 0.5,
-        width: 0.5
-      }}
-      position="-2 2.75 0"
-      material={{
-        color: 'green',
-        opacity: 0.3,
+      id="addItem"
+      position="0 0.4 0"
+      text={{
+        color: 'white',
+        align: 'center',
+        value: 'Add Item',
+        opacity: 1,
+        width: 3,
         side: 'double'
       }}
+    />
+    <Entity
+      geometry={{
+        primitive: 'box',
+        depth: 0.1,
+        height: 0.1,
+        width: 0.4
+      }}
+      material={{
+        color: 'green',
+        opacity: 0.9
+      }}
       shadow='receive: true;'
-      {...props}
-    >
-      <Entity id="addItem"
-              position="0 0.4 0"
-              text={{
-                color: 'white',
-                align: 'center',
-                value: 'Add Item',
-                opacity: 1,
-                width: 3,
-                side: 'double'
-              }}
-      />
-      <Entity
-        geometry={{
-          primitive: 'box',
-          depth: 0.1,
-          height: 0.1,
-          width: 0.4
-        }}
-        material={{
-          color: 'green',
-          opacity: 0.9
-        }}
-        shadow='receive: true;'
-      />
-      <Entity
-        geometry={{
-          primitive: 'box',
-          depth: 0.1,
-          height: 0.4,
-          width: 0.1
-        }}
-        material={{
-          color: 'green',
-          opacity: 0.9
-        }}
-        shadow='receive: true;'
-      />
-    </Entity>
+    />
+    <Entity
+      geometry={{
+        primitive: 'box',
+        depth: 0.1,
+        height: 0.4,
+        width: 0.1
+      }}
+      material={{
+        color: 'green',
+        opacity: 0.9
+      }}
+      shadow='receive: true;'
+    />
+  </Entity>
 );
 
 export default AddBlock;
--- client/src/components/SignUp/SignUp.js
+++ client/src/components/SignUp/SignUp.js
@@ -1,22 +1,22 @@
-import React, { Component } from "react";
-import API from "../../utils/API";
-import "./signUp.css";
-import { Redirect } from "react-router-dom";
+import React, { Component } from 'react';
+import API from '../../utils/API';
+import './signUp.css';
+import { Redirect } from 'react-router-dom';
 
 class SignUp extends Component {
   constructor() {
     super();
     this.state = {
-      username: "",
-      password: "",
-      passwordConfirm: "",
-      email: "",
+      username: '',
+      password: '',
+      passwordConfirm: '',
+      email: '',
       redirectTo: null,
       loggedIn: false,
-      usernameValidation: "",
-      passwordValidation: "",
-      emailValidation: "",
-      userError: ""
+      usernameValidation: '',
+      passwordValidation: '',
+      emailValidation: '',
+      userError: ''
     };
     this.handleFormSubmit = this.handleFormSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
@@ -40,33 +40,33 @@ class SignUp extends Component {
       userError: this.state.userError
     };
 
-    let reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+    const reg = /^([A-Za-z0-9_\-.])+@([A-Za-z0-9_\-.])+\.([A-Za-z]{2,4})$/;
 
     if (userInfo.password !== userInfo.passwordConfirm) {
       this.setState({
-        passwordValidation: "Your password and password confirmation do not match",
-        usernameValidation: "",
-        emailValidation: ""
+        passwordValidation: 'Your password and password confirmation do not match',
+        usernameValidation: '',
+        emailValidation: ''
       })
     }
     else if (!reg.test(userInfo.email)) {
       this.setState({
-        emailValidation: "Please enter a valid email address",
-        usernameValidation: "",
-        passwordValidation: ""
+        emailValidation: 'Please enter a valid email address',
+        usernameValidation: '',
+        passwordValidation: ''
       })    }
-    else if (userInfo.username === "") {
+    else if (userInfo.username === '') {
       this.setState({
-        usernameValidation: "Please enter a username",
-        passwordValidation: "",
-        emailValidation: ""
+        usernameValidation: 'Please enter a username',
+        passwordValidation: '',
+        emailValidation: ''
       })
     }
-    else if (userInfo.password === "") {
+    else if (userInfo.password === '') {
       this.setState({
-        passwordValidation: "Password is a required field",
-        usernameValidation: "",
-        emailValidation: ""
+        passwordValidation: 'Password is a required field',
+        usernameValidation: '',
+        emailValidation: ''
       })
     }
     else {
@@ -82,7 +82,7 @@ class SignUp extends Component {
               username: response.data.username,
               userRecordId: response.data._id
             });
-            console.log("successful signup!!");
+            console.log('successful signup!!');
             this.setState({
               redirectTo: '/todo'
             })
@@ -104,77 +104,77 @@ class SignUp extends Component {
     } else {
       return (
         <div>
-          <h1 id="signUpHeading">Sign Up</h1>
-          <div id="signUpForm">
+          <h1 id='signUpHeading'>Sign Up</h1>
+          <div id='signUpForm'>
             <form>
-
-              <div className="form-group">
-                <span className="validation" id="usernameValidate">{this.state.usernameValidation}</span>
+              <div className='form-group'>
+                <span className='validation' id='usernameValidate'>{this.state.usernameValidation}</span>
                 <br/>
-                <label htmlFor="exampleInputText">Username</label>
+                <label htmlFor='exampleInputText'>Username</label>
                 <input
-                  name="username"
-                  type="text"
-                  className="form-control"
-                  id="exampleInputText"
-                  aria-describedby="textHelp"
-                  placeholder="Enter username"
+                  name='username'
+                  type='text'
+                  className='form-control'
+                  id='exampleInputText'
+                  aria-describedby='textHelp'
+                  placeholder='Enter username'
                   value={this.state.username}
-                  onChange={this.handleChange} />
+                  onChange={this.handleChange}
+                />
               </div>
-              <div className="form-group">
-                <span className="validation" id="emailValidate">{this.state.emailValidation}</span>
+              <div className='form-group'>
+                <span className='validation' id='emailValidate'>{this.state.emailValidation}</span>
                 <br/>
-                <label htmlFor="exampleInputEmail1">Email</label>
+                <label htmlFor='exampleInputEmail1'>Email</label>
                 <input
-                  name="email"
-                  type="email"
-                  className="form-control"
-                  id="inputEmail"
+                  name='email'
+                  type='email'
+                  className='form-control'
+                  id='inputEmail'
                   value={this.state.email}
-                  placeholder="email@email.com"
-                  onChange={this.handleChange} />
-                <small id="textHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                  placeholder='email@email.com'
+                  onChange={this.handleChange}
+                />
+                <small id='textHelp' className='form-text text-muted'>
+                  We'll never share your email with anyone else.
+                </small>
               </div>
-              <div className="form-group">
-                <span className="validation" id="passwordValidate">{this.state.passwordValidation}</span>
+              <div className='form-group'>
+                <span className='validation' id='passwordValidate'>{this.state.passwordValidation}</span>
                 <br/>
-                <label htmlFor="exampleInputPassword1">Password</label>
+                <label htmlFor='exampleInputPassword1'>Password</label>
                 <input
-                  name="password"
-                  type="password"
-                  className="form-control"
-                  id="exampleInputPassword1"
+                  name='password'
+                  type='password'
+                  className='form-control'
+                  id='exampleInputPassword1'
                   value={this.state.password}
-                  placeholder="Password"
-                  onChange={this.handleChange} />
+                  placeholder='Password'
+                  onChange={this.handleChange}
+                />
               </div>
-              <div className="form-group">
+              <div className='form-group'>
               <br/>
-                <label htmlFor="exampleInputPassword2">Confirm Password</label>
+                <label htmlFor='exampleInputPassword2'>Confirm Password</label>
                 <input
-                  name="passwordConfirm"
-                  type="password"
-                  className="form-control"
-                  id="exampleInputPassword2"
+                  name='passwordConfirm'
+                  type='password'
+                  className='form-control'
+                  id='exampleInputPassword2'
                   value={this.state.passwordConfirm}
-                  placeholder="Password"
-                  onChange={this.handleChange} />
+                  placeholder='Password'
+                  onChange={this.handleChange}
+                />
               </div>
-
               <button
-                id="submit"
-                type="submit"
-                className="btn btn-primary"
+                id='submit'
+                type='submit'
+                className='btn btn-primary'
                 onClick={this.handleFormSubmit}
               >
                 Sign Up!
               </button>
-
-              
-
-              <span id="userError">{this.state.userError}</span>
-
+              <span id='userError'>{this.state.userError}</span>
             </form>
           </div>
         </div>
