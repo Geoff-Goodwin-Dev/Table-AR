fd87b7f35f0cd58a3b17095729666f64872e7a24
Geoff-Goodwin-Dev <ggoodwin.dev@gmail.com>
2018-07-28 09:15:05 -0400

    refactors Nav component

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,61 +1,32 @@
-import React, { Component } from "react";
-import "../../styles/Intro.css";
-import "./Nav.css";
+import React from 'react';
+import '../../styles/Intro.css';
+import './Nav.css';
 
-
-class Nav extends Component {
-  constructor() {
-    super();
-    this.state={
-      username: "",
-      password: "",
-    };
-
-  }
-
-  render() {
-    return (
-      <nav>
-        <ul>
-          <li>
-            <a
-              href={(this.props.loggedIn
-              ) ? (
-                ""
-              ) : (
-                "/"
-              )}
-              className="logo"
-            >
-              <h2>Table-AR</h2>
-            </a>
-          </li>
-
-          {(this.props.loggedIn
-          ) ? (
-            <li>
-              <button
-                type="button"
-                id="logOut"
-                className="btn btn-primary"
-                onClick={(event) => this.props.logout(event)}
-              >
-                LOGOUT
-              </button>
-            </li>
-          ) : (
-            <li/>
-          )
-          }
-
-          <li>
-            <a href="https://github.com/Geoff-Goodwin-Dev/Table-AR/tree/development">Github</a>
-          </li>
-        </ul>
-      </nav>
-    );
-  }
-}
-
-
-export default Nav;
+export const Nav = ({loggedIn}) => (
+  <nav>
+    <ul>
+      <li>
+        <a className='logo' href={(loggedIn) ? '' : '/'}>
+          <h2>Table-AR</h2>
+        </a>
+      </li>
+      {loggedIn ? (
+        <li>
+          <button
+            type='button'
+            id='logOut'
+            className='btn btn-primary'
+            onClick={(event) => this.props.logout(event)}
+          >
+            LOGOUT
+          </button>
+        </li>
+      ) :
+        null
+      }
+      <li>
+        <a href='https://github.com/Geoff-Goodwin-Dev/Table-AR/tree/development'>Github</a>
+      </li>
+    </ul>
+  </nav>
+);
