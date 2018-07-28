3b158e71dbd67289ba56a56f072122d81c80861b
Geoff-Goodwin-Dev <ggoodwin.dev@gmail.com>
2018-07-27 21:49:54 -0400

    refactors Login component and removes Jumbotron component

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,21 +1,20 @@
-import React, { Component } from "react";
-import API from "../../utils/API";
-import "./Login.css";
-import { Redirect } from "react-router-dom";
+import React, { Component } from 'react';
+import API from '../../utils/API';
+import './Login.css';
+import { Redirect } from 'react-router-dom';
 
-class Login extends Component {
+export default class Login extends Component {
   constructor() {
     super();
     this.state = {
-      username: "",
-      password: "",
+      username: '',
+      password: '',
       redirectTo: null,
       loggedIn: false,
-      loginFail: ""
+      loginFail: ''
     };
     this.handleFormLogin = this.handleFormLogin.bind(this);
     this.handleChange = this.handleChange.bind(this);
-
   }
 
   handleChange = event => {
@@ -25,13 +24,10 @@ class Login extends Component {
   };
 
   handleFormLogin = event => {
-    // Preventing the default behavior of the form submit (which is to refresh the page)
     event.preventDefault();
-
     console.log(`log-in-form, username: ${this.state.username}`);
 
     // request to server here
-
     let userInfo = {
       username: this.state.username,
       password: this.state.password
@@ -54,23 +50,22 @@ class Login extends Component {
       }).catch(error => {
         console.log(`login error: ${error}`);
 
-        if (this.state.username === "") {
+        if (this.state.username === '') {
           this.setState({
-            loginFail: "Please enter a valid username"
+            loginFail: 'Please enter a valid username'
           })
         }
-        else if (this.state.password === "") {
+        else if (this.state.password === '') {
           this.setState({
-            loginFail: "Please enter a valid password"
+            loginFail: 'Please enter a valid password'
           })
         }
         else if (error) {
           this.setState({
-            loginFail: "Username or password is not in our records"
+            loginFail: 'Username or password is not in our records'
           })
         }
       });
-
   };
 
   render() {
@@ -79,43 +74,41 @@ class Login extends Component {
     } else {
       return (
         <div>
-          <h1 id="loginHeading">Login</h1>
-          <br /><br /><br />
-          <div id="loginForm">
+          <h1 id='loginHeading'>Login</h1>
+          <div id='loginForm'>
             <form>
-              <div className="form-group">
-                <label htmlFor="exampleInputText">Username</label>
+              <div className='form-group'>
+                <label htmlFor='exampleInputText'>Username</label>
                 <input
-                  name="username"
-                  type="text"
-                  className="form-control"
-                  id="exampleInputText"
-                  aria-describedby="textHelp"
-                  placeholder="Enter username"
+                  name='username'
+                  type='text'
+                  className='form-control'
+                  id='exampleInputText'
+                  aria-describedby='textHelp'
+                  placeholder='Enter username'
                   value={this.state.username}
                   onChange={this.handleChange} />
               </div>
-              <div className="form-group">
-                <label htmlFor="exampleInputPassword1">Password</label>
+              <div className='form-group'>
+                <label htmlFor='exampleInputPassword1'>Password</label>
                 <input
-                  name="password"
-                  type="password"
-                  className="form-control"
-                  id="exampleInputPassword1"
+                  name='password'
+                  type='password'
+                  className='form-control'
+                  id='exampleInputPassword1'
                   value={this.state.password}
-                  placeholder="Password"
+                  placeholder='Password'
                   onChange={this.handleChange} />
               </div>
               <button
-                id="oldUser"
-                type="submit"
-                className="btn btn-primary"
+                id='oldUser'
+                type='submit'
+                className='btn btn-primary'
                 onClick={this.handleFormLogin}
               >
                 Login
-            </button>
-              <span id="loginFail">{this.state.loginFail}</span>
-
+              </button>
+              <span id='loginFail'>{this.state.loginFail}</span>
             </form>
           </div>
         </div>
@@ -123,5 +116,3 @@ class Login extends Component {
     }
   }
 }
-
-export default Login;
