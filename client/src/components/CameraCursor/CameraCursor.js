91000dbe00c7c7cffc49cecc87494f038a965ec0
Geoff-Goodwin-Dev <ggoodwin.dev@gmail.com>
2018-08-01 18:52:57 -0400

    refactors main page

--- client/src/components/CameraCursor/CameraCursor.js
+++ client/src/components/CameraCursor/CameraCursor.js
@@ -1,33 +1,30 @@
 import React from 'react';
 import { Entity } from 'aframe-react';
 
-export const CameraCursor = ({children}) => (
-  <Entity
-    primitive='a-camera'
-    wasd-controls-enabled='false'
-  >
-    <Entity
-      cursor={{fuse: true, fuseTimeout: 500}}
-      raycaster={{objects: '.clickable', far: 30}}
-      position='0 0 -2'
-      geometry='primitive: ring'
-      material='color: orange'
-      scale='.06 .06 .06'
-      animation__scale={{
-        property: 'scale',
-        dur: 500,
-        dir: 'alternate',
-        loop: 2,
-        easing: 'easeInCirc',
-        startEvents: 'fusing',
-        to: ' .01 .01 .01',
-      }}
-    />
-    <Entity
-      cursor={{
-        rayOrigin: 'mouse'
-      }}
-    />
+const CameraCursor = ({children}) => (
+  <Entity primitive='a-camera'
+          wasd-controls-enabled='false' >
+
+    <Entity cursor={{fuse: true, fuseTimeout: 500}}
+            raycaster={{objects: '.clickable', far: 30}}
+            position='0 0 -2'
+            geometry='primitive: ring'
+            material='color: orange'
+            scale='.06 .06 .06'
+            animation__scale={{
+              property: 'scale',
+              dur: 500,
+              dir: 'alternate',
+              loop: 2,
+              easing: 'easeInCirc',
+              startEvents: 'fusing',
+              to: ' .01 .01 .01',
+            }}/>
+    <Entity cursor={{rayOrigin: 'mouse'}} />
+
     {children}
+
   </Entity>
 );
+
+export default CameraCursor;
